169f6aa1a4224d0526cb1b62db5d1b2dd8e2113b
pj taron <jadepeck27@gmail.com>
2024-01-21 06:10:29 +0900

    add color to config

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      testestfdfsfadfasdfssssssssssss
-    </div>
+    <>
+      <header>
+        <Link className="text-red-600 font-semibold text-2xl"href="">ST PIZZA</Link>
+        <nav>
+          <Link href={''}>Home</Link>
+          <Link href={''}>Menu</Link>
+          <Link href={''}>About</Link>
+          <Link href={''}>Contact</Link>
+          <Link href={''}>Login</Link>
+        </nav>
+      </header>
+    </>
   )
 }
