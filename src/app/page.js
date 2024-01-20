c94d1f7f1f6f4181450cd95726afcd49ff7dd883
pj taron <jadepeck27@gmail.com>
2024-01-21 06:20:47 +0900

    header done

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <header>
-        <Link className="text-red-600 font-semibold text-2xl"href="">ST PIZZA</Link>
-        <nav>
+      <header className='flex items-center justify-between'>
+        <Link className="text-primary font-semibold text-2xl"href="">
+          ST PIZZA
+          </Link>
+        <nav className='flex items-center gap-8 text-gray-500 font-semibold'>
           <Link href={''}>Home</Link>
           <Link href={''}>Menu</Link>
           <Link href={''}>About</Link>
           <Link href={''}>Contact</Link>
-          <Link href={''}>Login</Link>
+          <Link href={''} className='bg-primary rounded-full text-white px-6 py-2 px-8'>
+            Login
+            </Link>
         </nav>
       </header>
     </>
