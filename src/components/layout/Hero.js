fc1bc7d7ea9f5b36573be81e798743e561591989
pj taron <jadepeck27@gmail.com>
2024-01-21 23:51:02 +0900

    change pic

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -6,8 +6,8 @@ export default function Hero() {
       <h1>Everything is better with a Pizza</h1>
       <p>Pizza is the missing piece that makes every day complete, a simple yet a delicious joy in life</p>
       <div className="w-16 h-16 relative">
-      <Image src={'/pizza1.png'} layout={'fill'} objectFit=
-      {'contain'} alt={'pizza1'}></Image>
+      <Image src={'/pizza.png'} layout={'fill'} objectFit=
+      {'contain'} alt={'pizza'}></Image>
       </div>
     </section>
   );
