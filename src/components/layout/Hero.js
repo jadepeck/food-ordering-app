b9b0b69facb2f5098eef2e36226ca65ed5e89d3c
pj taron <jadepeck27@gmail.com>
2024-01-21 23:58:22 +0900

    grid and columns

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -2,10 +2,16 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section>
+    <section className="grid grid-cols-2">
+      <div>
       <h1>Everything is better with a Pizza</h1>
-      <p>Pizza is the missing piece that makes every day complete, a simple yet a delicious joy in life</p>
-      <div className="w-16 h-16 relative">
+      <p>
+
+        Pizza is the missing piece that makes every day complete, a simple yet a delicious joy in life
+
+      </p>
+      </div>
+      <div className="relative">
       <Image src={'/pizza.png'} layout={'fill'} objectFit=
       {'contain'} alt={'pizza'}></Image>
       </div>
