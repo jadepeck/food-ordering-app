51a10cc82f4bc95d24b47d430aef366143d385c2
pj taron <jadepeck27@gmail.com>
2024-01-22 23:55:01 +0900

    make button

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -4,12 +4,20 @@ export default function Hero() {
   return (
     <section className="grid grid-cols-2">
       <div>
-      <h1 className="text-4xl font-semibold">Everything is better with a Pizza</h1>
-      <p>
+        <h1 className="text-4xl font-semibold">
 
-        Pizza is the missing piece that makes every day complete, a simple yet a delicious joy in life
+          Everything is better with a Pizza
 
-      </p>
+          </h1>
+        <p className="mt-4">
+
+          Pizza is the missing piece that makes every day complete, a simple yet a delicious joy in lifess
+
+        </p>
+       <div>
+         <button>Order now</button>
+         <button>Learn more</button>
+       </div>
       </div>
       <div className="relative">
       <Image src={'/pizza.png'} layout={'fill'} objectFit=
