3fbb0aedf6c2f2963bcb63a7f18e4790d1c5fb08
pj taron <jadepeck27@gmail.com>
2024-01-27 23:56:26 +0900

    section added on home menu

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,8 +1,10 @@
 export default function HomeMenu() {
   return (
-    <div className="text-center">
-      <h3>Check out</h3>
-      <h2>Menu</h2>
-    </div>
+    <section>
+      <div className="text-center">
+        <h3>Check out</h3>
+        <h2>Menu</h2>
+      </div>
+    </section>
   );
 }
