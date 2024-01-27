0b41be71624316eadb81ca45376b7aac47540ecd
pj taron <jadepeck27@gmail.com>
2024-01-27 23:58:07 +0900

    styling home menu

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -2,8 +2,8 @@ export default function HomeMenu() {
   return (
     <section>
       <div className="text-center">
-        <h3>Check out</h3>
-        <h2>Menu</h2>
+        <h3 className="uppercase text-grap-600 font-semibold">Check out</h3>
+        <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
       </div>
     </section>
   );
