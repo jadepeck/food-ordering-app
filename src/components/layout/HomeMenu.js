adb1691b7db3873a9dec941a30f8c7adf5330f6a
pj taron <jadepeck27@gmail.com>
2024-01-29 23:58:10 +0900

    salad pic added

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,9 +1,14 @@
+import Image from 'next/image';
+
 export default function HomeMenu() {
   return (
-    <section>
+    <section className="">
+      <div>
+        <Image src={'/sallad1.png'} width={'100'} height={'100'} alt={'sallad'} />
+      </div>
       <div className="text-center">
-        <h3 className="uppercase text-grap-600 font-semibold">Check out</h3>
-        <h2 className="text-primary font-bold text-4xl italic">Menu3</h2>
+        <h3 className="uppercase text-grap-600 font-semibold leading-4">Check out</h3>
+        <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
       </div>
     </section>
   );
