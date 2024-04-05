c95289bda58dfd2ea0532d75556661187fc5ae42
pj taron <jadepeck27@gmail.com>
2024-04-05 09:19:38 +0900

    update from bx

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -3,12 +3,17 @@ import Image from 'next/image';
 export default function HomeMenu() {
   return (
     <section className="">
-      <div>
-        <Image src={'/sallad1.png'} width={'100'} height={'100'} alt={'sallad'} />
+      <div className="h-48 relative">
+        <Image src={'/sallad1.png'} layout={'fill'}
+        objectFit={'contain'} alt={'sallad'} />
+      </div>
+      <div className="h-48 relative">
+        <Image src={'/sallad2.png'} layout={'fill'}
+        objectFit={'contain'} alt={'sallad'} />
       </div>
       <div className="text-center">
         <h3 className="uppercase text-grap-600 font-semibold leading-4">Check out</h3>
-        <h2 className="text-primary font-bold text-4xl italic">Menu3</h2>
+        <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
       </div>
     </section>
   );
