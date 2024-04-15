d96b7b28a008d6e9d1082fce82c4c75826f748c6
pj taron <jadepeck27@gmail.com>
2024-04-15 18:00:16 +0900

    adjusting salad

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -3,13 +3,15 @@ import Image from 'next/image';
 export default function HomeMenu() {
   return (
     <section className="">
-      <div className="h-48 relative">
-        <Image src={'/sallad1.png'} layout={'fill'}
-        objectFit={'contain'} alt={'sallad'} />
-      </div>
-      <div className="h-48 relative">
-        <Image src={'/sallad2.png'} layout={'fill'}
-        objectFit={'contain'} alt={'sallad'} />
+      <div className='absolute left-0 right-0 w-full'>
+        <div className="h-48 w-48 absolute text-left -left-12">
+          <Image src={'/sallad1.png'} layout={'fill'}
+          objectFit={'contain'} alt={'sallad'} />
+        </div>
+        <div className="h-48 w-48 absolute -right-12 -top-12">
+          <Image src={'/sallad2.png'} layout={'fill'}
+          objectFit={'contain'} alt={'sallad'} />
+        </div>
       </div>
       <div className="text-center">
         <h3 className="uppercase text-grap-600 font-semibold leading-4">Check out</h3>
